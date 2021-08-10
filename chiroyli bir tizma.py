c01367ec48e3a2607e2fe0976d273bf95f95401f
OLLIT.DEV <86409818+Gopporovoyatillo@users.noreply.github.com>
2021-08-10 21:28:00 +0500

    Add files via upload

---
+++ chiroyli bir tizma.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 10 21:26:42 2021
+
+@author: Admin
+"""
+
+import turtle
+turtle.bgcolor('black')
+squary=turtle.Turtle()
+squary.speed(20)
+squary.pencolor("red")
+for i in range(400):
+ squary.fd(i)
+ squary.lt(91)
+turtle.loop
